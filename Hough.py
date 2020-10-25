3320f199bd8514a4e18c3d7501a45e613a670b65
gurudasKarale <67753744+GurudasKarale@users.noreply.github.com>
2020-10-26 01:01:12 +0530

    Update Hough.py

--- Hough.py
+++ Hough.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv2.imread('C:/Users/Mohit K/Desktop/datasets/line.png', cv2.IMREAD_COLOR)
+img = cv2.imread('.../line.png', cv2.IMREAD_COLOR)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
